e299d73ac761fd8e2754a35ef70a2faac5060adc
Yoni-FDe <yoha1949@gmail.com>
2025-12-31 18:22:36 -0500

    update vite react

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:5000',
-//         changeOrigin: true,
-//       }
-//     }
-//   }
-// })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:4000',
-//         changeOrigin: true
-//       }
-//     }
-//   }
-// })
-
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/evayo7-frontend/", // 🔴 REQUIRED
+  base: "/evayo7-frontend/",   // ✅ CRITICAL FIX
 });
 
 
